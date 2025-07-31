a2c6b20d220205fbe42afd032149da57d06ac97d
Ilham Dohaan <doha39anilham@gmail.com>
2025-07-31 17:03:33 +0700

    ngarit boloo

--- src/tools/udp.js
+++ src/tools/udp.js
@@ -1,4 +1,5 @@
 const dgram = require('dgram');
+const net = require('net');
 
 const sendSignal = (message) => {
     const client = dgram.createSocket('udp4'); // BUAT socket baru
@@ -6,9 +7,19 @@ const sendSignal = (message) => {
         if (err) {
             // console.error('Gagal mengirim pesan:', err);
         } else {
-            // console.log('Pesan terkirim');
+            // console.log('Pesan terkirim' + message);
         }
         client.close();
+    });
+
+    const clientSocket = new net.Socket();
+    clientSocket.connect(6543, '127.0.0.1', () => {
+        clientSocket.write(message);
+        clientSocket.end();
+    });
+
+    clientSocket.on('error', (err) => {
+        console.error(`Terjadi error: ${err.message}`);
     });
 };
 
